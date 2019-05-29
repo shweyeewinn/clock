2993ddd4187f42b8f60f1ccd638b5d10f5bab8f5
shwe yee <yonngelay@gmail.com>
2019-05-29 17:28:46 +0800

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends React.Component {
+  state = {
+    time: new Date().toLocaleTimeString()
+  };
 
+  componentDidMount() {
+    setInterval(() => {
+      this.setState({
+        time: new Date().toLocaleTimeString()
+      });
+    }, 1000);
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">{this.state.time}</header>
+      </div>
+    );
+  }
+}
 export default App;
